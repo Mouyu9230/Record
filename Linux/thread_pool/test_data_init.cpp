a5217358e8d18c7fa88b4749d4b95db4e3720ec5
Mouyu9230 <2313228276@qq.com>
2026-04-14 20:51:59 +0800

    threadpool

--- Linux/thread_pool/test_data_init.cpp
+++ Linux/thread_pool/test_data_init.cpp
@@ -5,19 +5,19 @@ int main(){
     std::random_device rd;              // 真随机种子
     std::mt19937 gen(rd());    
 
-    std::uniform_int_distribution<int> dist(1, 100);
+    std::uniform_int_distribution<int> dist(1,20);
 
-    int rdnum[100];
+    int rdnum[50];
 
-    for(int i=0;i<100;i++){
+    for(int i=0;i<50;i++){
         int x = dist(gen);
         rdnum[i]=x;
     }
 
     FILE *fp = fopen("test_data.txt", "w");
     if (!fp) { printf("write failed\n"); return -1; }
-    for (int i = 0; i<100; i++)
-        fwrite(&rdnum[i], sizeof(int), 1, fp);
+    for (int i = 0; i<50; i++)
+        fprintf(fp,"%d\n",rdnum[i]);
     fclose(fp);
 
     return 0;
